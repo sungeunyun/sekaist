3a39a939c911aafcb2b3744c7c14269e0b4c3311
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2018-04-10 09:13:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 10384,
-	"created_at": "Sun Apr 08 00:13:06 +0000 2018",
+	"tweets": 10385,
+	"created_at": "Tue Apr 10 00:13:06 +0000 2018",
 	"lang": "ko"
 }
--- data/js/tweets/2018_04.js
+++ data/js/tweets/2018_04.js
@@ -1,6 +1,608 @@
 Grailbird.data.tweets_2018_04 = 
 [
 	{
+		"created_at": "Mon Apr 09 10:33:34 +0000 2018",
+		"id": 983291825933795300,
+		"id_str": "983291825933795328",
+		"text": "RT @Olympics: Spring 😗🌸#soohorang #PyeongChang2018 https://t.co/QKGWaaA4EI",
+		"truncated": false,
+		"entities": {
+			"hashtags": [
+				{
+					"text": "soohorang",
+					"indices": [
+						23,
+						33
+					]
+				},
+				{
+					"text": "PyeongChang2018",
+					"indices": [
+						34,
+						50
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Olympics",
+					"name": "Olympics",
+					"id": 52422878,
+					"id_str": "52422878",
+					"indices": [
+						3,
+						12
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 983285400063135700,
+					"id_str": "983285400063135744",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+					"url": "https://t.co/QKGWaaA4EI",
+					"display_url": "pic.twitter.com/QKGWaaA4EI",
+					"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983285493193420800,
+					"source_status_id_str": "983285493193420801",
+					"source_user_id": 52422878,
+					"source_user_id_str": "52422878"
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 983285400063135700,
+					"id_str": "983285400063135744",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+					"url": "https://t.co/QKGWaaA4EI",
+					"display_url": "pic.twitter.com/QKGWaaA4EI",
+					"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983285493193420800,
+					"source_status_id_str": "983285493193420801",
+					"source_user_id": 52422878,
+					"source_user_id_str": "52422878"
+				},
+				{
+					"id": 983285421814767600,
+					"id_str": "983285421814767618",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DaVU4agW4AImSes.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaVU4agW4AImSes.jpg",
+					"url": "https://t.co/QKGWaaA4EI",
+					"display_url": "pic.twitter.com/QKGWaaA4EI",
+					"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983285493193420800,
+					"source_status_id_str": "983285493193420801",
+					"source_user_id": 52422878,
+					"source_user_id_str": "52422878"
+				},
+				{
+					"id": 983285447131652100,
+					"id_str": "983285447131652097",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DaVU540X4AEJCrr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaVU540X4AEJCrr.jpg",
+					"url": "https://t.co/QKGWaaA4EI",
+					"display_url": "pic.twitter.com/QKGWaaA4EI",
+					"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 453,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1365,
+							"h": 2048,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983285493193420800,
+					"source_status_id_str": "983285493193420801",
+					"source_user_id": 52422878,
+					"source_user_id_str": "52422878"
+				},
+				{
+					"id": 983285477250928600,
+					"id_str": "983285477250928640",
+					"indices": [
+						51,
+						74
+					],
+					"media_url": "http://pbs.twimg.com/media/DaVU7pBXkAA-6Gb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DaVU7pBXkAA-6Gb.jpg",
+					"url": "https://t.co/QKGWaaA4EI",
+					"display_url": "pic.twitter.com/QKGWaaA4EI",
+					"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1339,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 445,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 785,
+							"resize": "fit"
+						}
+					},
+					"source_status_id": 983285493193420800,
+					"source_status_id_str": "983285493193420801",
+					"source_user_id": 52422878,
+					"source_user_id_str": "52422878"
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel 밴드 맥거핀 입덕하새오 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 599,
+			"friends_count": 1087,
+			"listed_count": 16,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 1385,
+			"utc_offset": 32400,
+			"time_zone": "Seoul",
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 10343,
+			"lang": "ko",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Mon Apr 09 10:08:24 +0000 2018",
+			"id": 983285493193420800,
+			"id_str": "983285493193420801",
+			"text": "Spring 😗🌸#soohorang #PyeongChang2018 https://t.co/QKGWaaA4EI",
+			"truncated": false,
+			"entities": {
+				"hashtags": [
+					{
+						"text": "soohorang",
+						"indices": [
+							9,
+							19
+						]
+					},
+					{
+						"text": "PyeongChang2018",
+						"indices": [
+							20,
+							36
+						]
+					}
+				],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 983285400063135700,
+						"id_str": "983285400063135744",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+						"url": "https://t.co/QKGWaaA4EI",
+						"display_url": "pic.twitter.com/QKGWaaA4EI",
+						"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 983285400063135700,
+						"id_str": "983285400063135744",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaVU3JeXUAAT1XB.jpg",
+						"url": "https://t.co/QKGWaaA4EI",
+						"display_url": "pic.twitter.com/QKGWaaA4EI",
+						"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 983285421814767600,
+						"id_str": "983285421814767618",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DaVU4agW4AImSes.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaVU4agW4AImSes.jpg",
+						"url": "https://t.co/QKGWaaA4EI",
+						"display_url": "pic.twitter.com/QKGWaaA4EI",
+						"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1365,
+								"h": 2048,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 983285447131652100,
+						"id_str": "983285447131652097",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DaVU540X4AEJCrr.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaVU540X4AEJCrr.jpg",
+						"url": "https://t.co/QKGWaaA4EI",
+						"display_url": "pic.twitter.com/QKGWaaA4EI",
+						"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 453,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 800,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1365,
+								"h": 2048,
+								"resize": "fit"
+							}
+						}
+					},
+					{
+						"id": 983285477250928600,
+						"id_str": "983285477250928640",
+						"indices": [
+							37,
+							60
+						],
+						"media_url": "http://pbs.twimg.com/media/DaVU7pBXkAA-6Gb.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/DaVU7pBXkAA-6Gb.jpg",
+						"url": "https://t.co/QKGWaaA4EI",
+						"display_url": "pic.twitter.com/QKGWaaA4EI",
+						"expanded_url": "https://twitter.com/Olympics/status/983285493193420801/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 2048,
+								"h": 1339,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 445,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 785,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 52422878,
+				"id_str": "52422878",
+				"name": "Olympics",
+				"screen_name": "Olympics",
+				"location": "Lausanne, Switzerland",
+				"description": "The Olympic Games",
+				"url": "https://t.co/h3M6SFQ1oN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/h3M6SFQ1oN",
+								"expanded_url": "http://www.olympic.org",
+								"display_url": "olympic.org",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 6283708,
+				"friends_count": 4784,
+				"listed_count": 12283,
+				"created_at": "Tue Jun 30 15:23:29 +0000 2009",
+				"favourites_count": 3198,
+				"utc_offset": 7200,
+				"time_zone": "Paris",
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 6701,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "637586",
+				"profile_background_image_url": "http://pbs.twimg.com/profile_background_images/451013857545183232/iEOsn__h.jpeg",
+				"profile_background_image_url_https": "https://pbs.twimg.com/profile_background_images/451013857545183232/iEOsn__h.jpeg",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/975372356960096256/mHGbWlPT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/975372356960096256/mHGbWlPT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/52422878/1512122817",
+				"profile_link_color": "0069AF",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E5E7E9",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1521,
+			"favorite_count": 3576,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 1521,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Sat Apr 07 14:14:05 +0000 2018",
 		"id": 982622544694931500,
 		"id_str": "982622544694931456",
