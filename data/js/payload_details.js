f7f9cb0c92b087ff438715f1eccf60fe40cb1baa
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2019-08-08 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 12907,
-	"created_at": "Wed Aug 07 00:13:05 +0000 2019",
+	"tweets": 12908,
+	"created_at": "Thu Aug 08 00:13:05 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 07 10:38:24 +0000 2019",
+		"id": 1159021844923441200,
+		"id_str": "1159051155718021120",
+		"full_text": "&lt;우리집&gt; 보고 나와 찬찬히 보도자료를 살피다가 영화 소개 바로 다음장에 촬영 수칙이 인상적이라 공유한다. ‘어린이 배우들과 함께하는 성인분들께 드리는 당부의 말’이라는 설명이 달려있는데, 그냥 어린이들과 함께하는 성인들에게 하는 말이기도 할 것. https://t.co/fadIwuPkWp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			144
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1159021833917616000,
+					"id_str": "1159021833917616130",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBWsN5mU0AIEkd8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBWsN5mU0AIEkd8.jpg",
+					"url": "https://t.co/fadIwuPkWp",
+					"display_url": "pic.twitter.com/fadIwuPkWp",
+					"expanded_url": "https://twitter.com/Yooninas/status/1159021844923441152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1159021833917616000,
+					"id_str": "1159021833917616130",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBWsN5mU0AIEkd8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBWsN5mU0AIEkd8.jpg",
+					"url": "https://t.co/fadIwuPkWp",
+					"display_url": "pic.twitter.com/fadIwuPkWp",
+					"expanded_url": "https://twitter.com/Yooninas/status/1159021844923441152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1159021833921843200,
+					"id_str": "1159021833921843200",
+					"indices": [
+						145,
+						168
+					],
+					"media_url": "http://pbs.twimg.com/media/EBWsN5nVUAAciW1.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EBWsN5nVUAAciW1.jpg",
+					"url": "https://t.co/fadIwuPkWp",
+					"display_url": "pic.twitter.com/fadIwuPkWp",
+					"expanded_url": "https://twitter.com/Yooninas/status/1159021844923441152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 470,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 830,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1416,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 941037213969559600,
+			"id_str": "941037213969559552",
+			"name": "Yoonina",
+			"screen_name": "Yooninas",
+			"location": "",
+			"description": "“But I can only do what feels right during these difficult times of transition and collective consciousness raising.”",
+			"url": "https://t.co/8QxDkGdynm",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8QxDkGdynm",
+							"expanded_url": "http://m.podbbang.com/ch/1771727",
+							"display_url": "m.podbbang.com/ch/1771727",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1357,
+			"friends_count": 299,
+			"listed_count": 7,
+			"created_at": "Wed Dec 13 20:08:50 +0000 2017",
+			"favourites_count": 2300,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 3283,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/983306799225778177/7HPxLuIm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/983306799225778177/7HPxLuIm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/941037213969559552/1548516965",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3540,
+		"favorite_count": 1976,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "&lt;우리집&gt; 보고 나와 찬찬히 보도자료를 살피다가 영화 소개 바로 다음장에 촬영 수칙이 인상적이라 공유한다. ‘어린이 배우들과 함께하는 성인분들께 드리는 당부의 말’이라는 설명이 달려있는데, 그냥 어린이들과 함께하는 성인들에게 하는 말이기도 할 것. https://t.co/fadIwuPkWp"
+	},
+	{
 		"created_at": "Tue Aug 06 04:54:55 +0000 2019",
 		"id": 1158598393175740400,
 		"id_str": "1158602327101591552",
