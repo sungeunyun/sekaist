05964dde7888126c3cff2f2bda3c9907243bc84a
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2019-12-03 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13674,
-	"created_at": "Mon Dec 02 00:13:07 +0000 2019",
+	"tweets": 13675,
+	"created_at": "Tue Dec 03 00:13:06 +0000 2019",
 	"lang": "ko"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,197 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 02 11:18:51 +0000 2019",
+		"id": 1201423187906416600,
+		"id_str": "1201460715980120064",
+		"full_text": "홀로그램 필름의 기존 멤버 4인이 새로운 이름으로 다시 시작합니다. 풀인더풀 많은 기대 바랍니다. https://t.co/b3y4uVF1n8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			54
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1201423177282248700,
+					"id_str": "1201423177282248705",
+					"indices": [
+						55,
+						78
+					],
+					"media_url": "http://pbs.twimg.com/media/EKxQA6rUYAE0f5k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKxQA6rUYAE0f5k.jpg",
+					"url": "https://t.co/b3y4uVF1n8",
+					"display_url": "pic.twitter.com/b3y4uVF1n8",
+					"expanded_url": "https://twitter.com/FITP_official/status/1201423187906416640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1201423177282248700,
+					"id_str": "1201423177282248705",
+					"indices": [
+						55,
+						78
+					],
+					"media_url": "http://pbs.twimg.com/media/EKxQA6rUYAE0f5k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EKxQA6rUYAE0f5k.jpg",
+					"url": "https://t.co/b3y4uVF1n8",
+					"display_url": "pic.twitter.com/b3y4uVF1n8",
+					"expanded_url": "https://twitter.com/FITP_official/status/1201423187906416640/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 389575531,
+			"id_str": "389575531",
+			"name": "FOOL IN THE POOL",
+			"screen_name": "FITP_official",
+			"location": "",
+			"description": "We can’t swim",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1567,
+			"friends_count": 418,
+			"listed_count": 29,
+			"created_at": "Wed Oct 12 17:09:57 +0000 2011",
+			"favourites_count": 4,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 2120,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1201227551185616896/kAPMt8Ma_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1201227551185616896/kAPMt8Ma_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/389575531/1446646383",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 21,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "홀로그램 필름의 기존 멤버 4인이 새로운 이름으로 다시 시작합니다. 풀인더풀 많은 기대 바랍니다. https://t.co/b3y4uVF1n8"
+	},
+	{
 		"created_at": "Sun Dec 01 23:34:52 +0000 2019",
 		"id": 1201283552618303500,
 		"id_str": "1201283552618303490",
