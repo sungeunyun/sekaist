c99785f376b9518bec12870dc9ad4000d91ed391
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2020-05-25 09:13:09 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15458,
-	"created_at": "Sun May 24 00:13:08 +0000 2020",
+	"tweets": 15459,
+	"created_at": "Mon May 25 00:13:08 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 24 14:15:44 +0000 2020",
+		"id": 1264541775672119300,
+		"id_str": "1264560718793203712",
+		"full_text": "펭수.. 배신자,, 너무 잘추잖아..🕺\n\n펭수 틱톡 계정 : https://t.co/nzsf6e7g52\n\n#펭수 #리아킴 #LIAKIM #PENGSOO #TikTok https://t.co/Zhcw0ZgqC5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			92
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "펭수",
+					"indices": [
+						59,
+						62
+					]
+				},
+				{
+					"text": "리아킴",
+					"indices": [
+						63,
+						67
+					]
+				},
+				{
+					"text": "LIAKIM",
+					"indices": [
+						68,
+						75
+					]
+				},
+				{
+					"text": "PENGSOO",
+					"indices": [
+						76,
+						84
+					]
+				},
+				{
+					"text": "TikTok",
+					"indices": [
+						85,
+						92
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/nzsf6e7g52",
+					"expanded_url": "https://vt.tiktok.com/BrYdJW/",
+					"display_url": "vt.tiktok.com/BrYdJW/",
+					"indices": [
+						34,
+						57
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1264541720403800000,
+					"id_str": "1264541720403800064",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1264541720403800064/pu/img/8bKrUMS6z9-vrnkM.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1264541720403800064/pu/img/8bKrUMS6z9-vrnkM.jpg",
+					"url": "https://t.co/Zhcw0ZgqC5",
+					"display_url": "pic.twitter.com/Zhcw0ZgqC5",
+					"expanded_url": "https://twitter.com/TiktokKR/status/1264541775672119296/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 578,
+							"h": 1022,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 385,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 578,
+							"h": 1022,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1264541720403800000,
+					"id_str": "1264541720403800064",
+					"indices": [
+						93,
+						116
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1264541720403800064/pu/img/8bKrUMS6z9-vrnkM.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1264541720403800064/pu/img/8bKrUMS6z9-vrnkM.jpg",
+					"url": "https://t.co/Zhcw0ZgqC5",
+					"display_url": "pic.twitter.com/Zhcw0ZgqC5",
+					"expanded_url": "https://twitter.com/TiktokKR/status/1264541775672119296/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 578,
+							"h": 1022,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 385,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 578,
+							"h": 1022,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							289,
+							511
+						],
+						"duration_millis": 14582,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1264541720403800064/pu/vid/578x1022/5LFzoTHQrWp8OeoK.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1264541720403800064/pu/pl/KOOec47nyiPUO9XY.m3u8?tag=10"
+							},
+							{
+								"bitrate": 632000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1264541720403800064/pu/vid/320x564/1hJUTDjmlwbXJtjL.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1264541720403800064/pu/vid/360x636/yKLGM1iyKF47mDVs.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://www.smartpost.kr/\" rel=\"nofollow\">smartpost.kr</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 895890154639966200,
+			"id_str": "895890154639966208",
+			"name": "TikTok_Korea",
+			"screen_name": "TiktokKR",
+			"location": "",
+			"description": "짧아서 확실한 행복, 숏.확.행 \nReal People. Real Videos.",
+			"url": "https://t.co/JUSOaPuchF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/JUSOaPuchF",
+							"expanded_url": "https://www.tiktok.com/ko/",
+							"display_url": "tiktok.com/ko/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 161988,
+			"friends_count": 508,
+			"listed_count": 75,
+			"created_at": "Fri Aug 11 06:10:32 +0000 2017",
+			"favourites_count": 463,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1568,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087944986509881345/qZi1Fn-m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087944986509881345/qZi1Fn-m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/895890154639966208/1580959453",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 57,
+		"favorite_count": 395,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "펭수.. 배신자,, 너무 잘추잖아..🕺\n\n펭수 틱톡 계정 : https://t.co/nzsf6e7g52\n\n#펭수 #리아킴 #LIAKIM #PENGSOO #TikTok https://t.co/Zhcw0ZgqC5"
+	},
+	{
 		"created_at": "Sat May 23 16:21:30 +0000 2020",
 		"id": 1264229981162356700,
 		"id_str": "1264229981162356737",
