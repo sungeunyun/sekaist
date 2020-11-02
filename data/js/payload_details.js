7b224496d64a4120108adc73ab3b8cf03a1285f1
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2020-11-02 09:13:07 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16605,
-	"created_at": "Sun Nov 01 00:13:08 +0000 2020",
+	"tweets": 16607,
+	"created_at": "Mon Nov 02 00:13:06 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_11.js
@@ -0,0 +1,288 @@
+Grailbird.data.tweets_2020_11 = 
+[
+	{
+		"created_at": "Sun Nov 01 13:29:20 +0000 2020",
+		"id": 1322880649607307300,
+		"id_str": "1322893488480907266",
+		"full_text": "201108 SBS 집사부일체\n이런 대형떡밥.. 물어도 될까요..ㅜㅜ https://t.co/IiHW6d2Iaz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			38
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1322880552098095000,
+					"id_str": "1322880552098095105",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1322880552098095105/pu/img/sNBGCoW3knE2B6UZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1322880552098095105/pu/img/sNBGCoW3knE2B6UZ.jpg",
+					"url": "https://t.co/IiHW6d2Iaz",
+					"display_url": "pic.twitter.com/IiHW6d2Iaz",
+					"expanded_url": "https://twitter.com/juck_jjal/status/1322880649607307266/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1322880552098095000,
+					"id_str": "1322880552098095105",
+					"indices": [
+						39,
+						62
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1322880552098095105/pu/img/sNBGCoW3knE2B6UZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1322880552098095105/pu/img/sNBGCoW3knE2B6UZ.jpg",
+					"url": "https://t.co/IiHW6d2Iaz",
+					"display_url": "pic.twitter.com/IiHW6d2Iaz",
+					"expanded_url": "https://twitter.com/juck_jjal/status/1322880649607307266/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1280,
+							"h": 720,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 46596,
+						"variants": [
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1322880552098095105/pu/vid/1280x720/hLaeeGctZBdwrjOV.mp4?tag=10"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1322880552098095105/pu/pl/S54IxQWxPDBHvTqJ.m3u8?tag=10"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1322880552098095105/pu/vid/480x270/X6c1gzLbYoTe_w2E.mp4?tag=10"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1322880552098095105/pu/vid/640x360/6sN1o_GTu5Gd9xli.mp4?tag=10"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 749241755732586500,
+			"id_str": "749241755732586497",
+			"name": "적짤봇",
+			"screen_name": "juck_jjal",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 71,
+			"friends_count": 41,
+			"listed_count": 1,
+			"created_at": "Sat Jul 02 14:02:09 +0000 2016",
+			"favourites_count": 18,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 691,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1089202057901355008/cnVG0f4E_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1089202057901355008/cnVG0f4E_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/749241755732586497/1483542755",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 12,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "201108 SBS 집사부일체\n이런 대형떡밥.. 물어도 될까요..ㅜㅜ https://t.co/IiHW6d2Iaz"
+	},
+	{
+		"created_at": "Sun Nov 01 08:00:38 +0000 2020",
+		"id": 1322810766492196900,
+		"id_str": "1322810766492196864",
+		"full_text": "＂먹는 것보다 좋은 게 어디 있어요＂ 섹시 빌런의 본체는 먹는 것에 진심? 유태오 인터뷰 (출처 : 씨네플레이 | 네이버TV) https://t.co/iPqkU6Gs6j",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			94
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iPqkU6Gs6j",
+					"expanded_url": "http://naver.me/GZMwejsc",
+					"display_url": "naver.me/GZMwejsc",
+					"indices": [
+						71,
+						94
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel 맥거핀 유태오 그리고 펭수🐧💙 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 621,
+			"friends_count": 1099,
+			"listed_count": 14,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 1970,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 16602,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "＂먹는 것보다 좋은 게 어디 있어요＂ 섹시 빌런의 본체는 먹는 것에 진심? 유태오 인터뷰 (출처 : 씨네플레이 | 네이버TV) https://t.co/iPqkU6Gs6j"
+	}
+]
