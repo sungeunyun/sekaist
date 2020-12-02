c9f2dbe4a819e69620d079c42ca88e9abf91c9bc
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2020-12-02 09:13:08 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16843,
-	"created_at": "Tue Dec 01 00:13:08 +0000 2020",
+	"tweets": 16844,
+	"created_at": "Wed Dec 02 00:13:07 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,109 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Tue Dec 01 05:39:38 +0000 2020",
+		"id": 1333607044763435000,
+		"id_str": "1333646920145768448",
+		"full_text": "개막작 &lt;The CMR(더 씨엠알)&gt; \n드디어 공개!! \n클릭 GOGO!! \n\n감독: 권호영, 김영남, 박진순, 봉만대, 신아가, 심찬양, 안상훈, 오점균, 이서, 이옥섭, 이종훈, 임선애, 정용주, 진승현, 황욱\n\nhttps://t.co/99ltohytub",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			150
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/99ltohytub",
+					"expanded_url": "https://tv.naver.com/v/16966452",
+					"display_url": "tv.naver.com/v/16966452",
+					"indices": [
+						127,
+						150
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1332636173798318000,
+		"in_reply_to_status_id_str": "1332636173798318084",
+		"in_reply_to_user_id": 3281256852,
+		"in_reply_to_user_id_str": "3281256852",
+		"in_reply_to_screen_name": "TheCMR_official",
+		"user": {
+			"id": 3281256852,
+			"id_str": "3281256852",
+			"name": "충무로영화제-디렉터스 위크",
+			"screen_name": "TheCMR_official",
+			"location": "대한민국 중구",
+			"description": "제5회 충무로영화제🎬 2020.12.1(화) -5(토)",
+			"url": "https://t.co/eiysnxgYYt",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/eiysnxgYYt",
+							"expanded_url": "http://thecmr.kr/",
+							"display_url": "thecmr.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1070,
+			"friends_count": 971,
+			"listed_count": 12,
+			"created_at": "Thu Jul 16 05:56:32 +0000 2015",
+			"favourites_count": 478,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1406,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1324182479813533697/XgajrtQS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1324182479813533697/XgajrtQS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3281256852/1603170406",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "개막작 &lt;The CMR(더 씨엠알)&gt; \n드디어 공개!! \n클릭 GOGO!! \n\n감독: 권호영, 김영남, 박진순, 봉만대, 신아가, 심찬양, 안상훈, 오점균, 이서, 이옥섭, 이종훈, 임선애, 정용주, 진승현, 황욱\n\nhttps://t.co/99ltohytub"
+	}
+]
