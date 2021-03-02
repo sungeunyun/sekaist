a4b99ea4088ff2d8ea8efb89388ae3fe63f5d1c3
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2021-03-02 09:13:28 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17493,
-	"created_at": "Mon Mar 01 00:13:08 +0000 2021",
+	"tweets": 17494,
+	"created_at": "Tue Mar 02 00:13:14 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_03.js
@@ -0,0 +1,196 @@
+Grailbird.data.tweets_2021_03 = 
+[
+	{
+		"created_at": "Mon Mar 01 02:59:14 +0000 2021",
+		"id": 1366218945859059700,
+		"id_str": "1366221460759482370",
+		"full_text": "Congratulations to Minari (USA) (@MinariMovie) - Best Motion Picture - Foreign Language. - #GoldenGlobes https://t.co/ARFFIVN8GP",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "GoldenGlobes",
+					"indices": [
+						91,
+						104
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MinariMovie",
+					"name": "Minari",
+					"id": 1215725544492425200,
+					"id_str": "1215725544492425216",
+					"indices": [
+						33,
+						45
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1366218825654558700,
+					"id_str": "1366218825654558721",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EvXIyIyXYAEeSkY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvXIyIyXYAEeSkY.jpg",
+					"url": "https://t.co/ARFFIVN8GP",
+					"display_url": "pic.twitter.com/ARFFIVN8GP",
+					"expanded_url": "https://twitter.com/goldenglobes/status/1366218945859059712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1366218825654558700,
+					"id_str": "1366218825654558721",
+					"indices": [
+						105,
+						128
+					],
+					"media_url": "http://pbs.twimg.com/media/EvXIyIyXYAEeSkY.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EvXIyIyXYAEeSkY.jpg",
+					"url": "https://t.co/ARFFIVN8GP",
+					"display_url": "pic.twitter.com/ARFFIVN8GP",
+					"expanded_url": "https://twitter.com/goldenglobes/status/1366218945859059712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 600,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 340,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 18667907,
+			"id_str": "18667907",
+			"name": "Golden Globe Awards",
+			"screen_name": "goldenglobes",
+			"location": "Hollywood, California",
+			"description": "The Official Twitter Account for the Hollywood Foreign Press Association (HFPA) and the Golden Globe® Awards. Catch the 78th #GoldenGlobes 2/28 on @nbc!",
+			"url": "https://t.co/l079QFNhrF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/l079QFNhrF",
+							"expanded_url": "http://www.goldenglobes.com",
+							"display_url": "goldenglobes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1352526,
+			"friends_count": 287,
+			"listed_count": 5359,
+			"created_at": "Tue Jan 06 08:59:41 +0000 2009",
+			"favourites_count": 939,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12269,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1084164700378521600/K4wEiiR6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1084164700378521600/K4wEiiR6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/18667907/1614038924",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F3F0D4",
+			"profile_text_color": "444444",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4213,
+		"favorite_count": 11138,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Congratulations to Minari (USA) (@MinariMovie) - Best Motion Picture - Foreign Language. - #GoldenGlobes https://t.co/ARFFIVN8GP"
+	}
+]
