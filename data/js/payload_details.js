f1c78c234a93ba1d141b4acf5e6a234bca6b4cbe
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2021-09-02 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18483,
-	"created_at": "Wed Sep 01 00:13:05 +0000 2021",
+	"tweets": 18484,
+	"created_at": "Thu Sep 02 00:13:05 +0000 2021",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2021_09.js
@@ -0,0 +1,214 @@
+Grailbird.data.tweets_2021_09 = 
+[
+	{
+		"created_at": "Wed Sep 01 10:15:27 +0000 2021",
+		"id": 1433010421712097300,
+		"id_str": "1433010606114676741",
+		"full_text": "석달 전 &lt;마루&gt;와 &lt;곰이&gt; 사이에서 \n태어난 풍산개 새끼 7마리가 \n모두 튼튼하게 자랐습니다. \n많은 분들이 보내주신 의견에 따라 \n이름을 &lt;아름&gt;, &lt;다운&gt;, &lt;강산&gt;, &lt;봄&gt;, \n&lt;여름&gt;, &lt;가을&gt;, &lt;겨울&gt;로 지었습니다. https://t.co/f2vPzn4yrX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			181
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1433008010868117500,
+					"id_str": "1433008010868117511",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MRMYgVUAcKGO4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MRMYgVUAcKGO4.jpg",
+					"url": "https://t.co/f2vPzn4yrX",
+					"display_url": "pic.twitter.com/f2vPzn4yrX",
+					"expanded_url": "https://twitter.com/moonriver365/status/1433010421712097285/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1433008010868117500,
+					"id_str": "1433008010868117511",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MRMYgVUAcKGO4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MRMYgVUAcKGO4.jpg",
+					"url": "https://t.co/f2vPzn4yrX",
+					"display_url": "pic.twitter.com/f2vPzn4yrX",
+					"expanded_url": "https://twitter.com/moonriver365/status/1433010421712097285/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1433008010868134000,
+					"id_str": "1433008010868133888",
+					"indices": [
+						182,
+						205
+					],
+					"media_url": "http://pbs.twimg.com/media/E-MRMYgVkAA8WMW.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/E-MRMYgVkAA8WMW.jpg",
+					"url": "https://t.co/f2vPzn4yrX",
+					"display_url": "pic.twitter.com/f2vPzn4yrX",
+					"expanded_url": "https://twitter.com/moonriver365/status/1433010421712097285/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 829,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 470,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1415,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 444465942,
+			"id_str": "444465942",
+			"name": "문재인",
+			"screen_name": "moonriver365",
+			"location": "대한민국",
+			"description": "대한민국 대통령 문재인입니다. President of the Republic of Korea. 나라다운 나라를 만들기 위해 국민여러분과 함께 하겠습니다. @thebluehousekr @TheBlueHouseENG",
+			"url": "https://t.co/FAKWOwyhHd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/FAKWOwyhHd",
+							"expanded_url": "http://www.president.go.kr",
+							"display_url": "president.go.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1874730,
+			"friends_count": 140939,
+			"listed_count": 7598,
+			"created_at": "Fri Dec 23 08:46:08 +0000 2011",
+			"favourites_count": 718,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 4284,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5ABB5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/798463233774350336/KlHqUNgL_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/798463233774350336/KlHqUNgL_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/444465942/1583716608",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": true,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9627,
+		"favorite_count": 12197,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "석달 전 &lt;마루&gt;와 &lt;곰이&gt; 사이에서 \n태어난 풍산개 새끼 7마리가 \n모두 튼튼하게 자랐습니다. \n많은 분들이 보내주신 의견에 따라 \n이름을 &lt;아름&gt;, &lt;다운&gt;, &lt;강산&gt;, &lt;봄&gt;, \n&lt;여름&gt;, &lt;가을&gt;, &lt;겨울&gt;로 지었습니다. https://t.co/f2vPzn4yrX"
+	}
+]
