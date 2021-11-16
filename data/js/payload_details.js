a686ba43db173c63a0898a11523ff6330fdd120d
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2021-11-16 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18648,
-	"created_at": "Mon Nov 15 00:13:05 +0000 2021",
+	"tweets": 18649,
+	"created_at": "Tue Nov 16 00:13:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_11.js
+++ data/js/tweets/2021_11.js
@@ -1,6 +1,189 @@
 Grailbird.data.tweets_2021_11 = 
 [
 	{
+		"created_at": "Mon Nov 15 10:06:20 +0000 2021",
+		"id": 1460170708894445600,
+		"id_str": "1460187403902021635",
+		"full_text": "#웨스앤더슨 감독의 &lt;프렌치 디스패치&gt;, 소장 욕구 100%\n11월 18일부터 만나는 극장 스페셜 굿즈 이벤트!\n \n* 자세한 진행 극장 및 유의사항은 각 극장 홈페이지, 어플을 통해 확인 부탁 드립니다. https://t.co/DQmRVg9HNO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			120
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "웨스앤더슨",
+					"indices": [
+						0,
+						6
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1459100491200475100,
+					"id_str": "1459100491200475136",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FD_EKoWaMAAUlnK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FD_EKoWaMAAUlnK.jpg",
+					"url": "https://t.co/DQmRVg9HNO",
+					"display_url": "pic.twitter.com/DQmRVg9HNO",
+					"expanded_url": "https://twitter.com/20thcenturyKR/status/1460170708894445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1459100491200475100,
+					"id_str": "1459100491200475136",
+					"indices": [
+						121,
+						144
+					],
+					"media_url": "http://pbs.twimg.com/media/FD_EKoWaMAAUlnK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FD_EKoWaMAAUlnK.jpg",
+					"url": "https://t.co/DQmRVg9HNO",
+					"display_url": "pic.twitter.com/DQmRVg9HNO",
+					"expanded_url": "https://twitter.com/20thcenturyKR/status/1460170708894445569/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1100,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 668,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1100,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 199174562,
+			"id_str": "199174562",
+			"name": "이십세기 스튜디오 코리아",
+			"screen_name": "20thcenturyKR",
+			"location": "Korea",
+			"description": "이십세기 스튜디오 코리아 공식 트위터 입니다",
+			"url": "https://t.co/BnlEQLfQvM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/BnlEQLfQvM",
+							"expanded_url": "http://www.facebook.com/20thCenturyStudios.KR",
+							"display_url": "facebook.com/20thCenturyStu…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 18950,
+			"friends_count": 14135,
+			"listed_count": 195,
+			"created_at": "Wed Oct 06 06:00:32 +0000 2010",
+			"favourites_count": 128,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 5055,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1227049976565358593/7WUKn3hw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1227049976565358593/7WUKn3hw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/199174562/1635902787",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "F3F3F3",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 19,
+		"favorite_count": 22,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "#웨스앤더슨 감독의 &lt;프렌치 디스패치&gt;, 소장 욕구 100%\n11월 18일부터 만나는 극장 스페셜 굿즈 이벤트!\n \n* 자세한 진행 극장 및 유의사항은 각 극장 홈페이지, 어플을 통해 확인 부탁 드립니다. https://t.co/DQmRVg9HNO"
+	},
+	{
 		"created_at": "Sun Nov 14 02:08:10 +0000 2021",
 		"id": 1459562297618141200,
 		"id_str": "1459704680771031041",
