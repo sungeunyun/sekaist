f83db0be17b9c13b00bda85c74f9d28df032bb5e
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2021-12-03 09:13:06 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18698,
-	"created_at": "Thu Dec 02 00:13:05 +0000 2021",
+	"tweets": 18700,
+	"created_at": "Fri Dec 03 00:13:05 +0000 2021",
 	"lang": "ko"
 }
--- data/js/tweets/2021_12.js
+++ data/js/tweets/2021_12.js
@@ -1,6 +1,341 @@
 Grailbird.data.tweets_2021_12 = 
 [
 	{
+		"created_at": "Thu Dec 02 06:30:48 +0000 2021",
+		"id": 1466293757247033300,
+		"id_str": "1466293757247033346",
+		"full_text": "오늘은 라이카만 잘잡으면 성공적",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			17
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel   맥거핀 유태오 그리고 펭수🐧💙 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 627,
+			"friends_count": 1130,
+			"listed_count": 14,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 2103,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18920,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "오늘은 라이카만 잘잡으면 성공적"
+	},
+	{
+		"created_at": "Thu Dec 02 01:57:12 +0000 2021",
+		"id": 1466000952389894100,
+		"id_str": "1466224903045087239",
+		"full_text": "영화의 모든 순간에 당신이 있었다👨‍👩‍👧‍👦\n\n#도서관같은곳 #공부하듯이다녔던\n#희망을주는것 #새로운자극 #애인같은존재\n\n영화인들의 릴레이 인터뷰 미리보기👀\n\n** 고레에다 히로카즈 감독님 특별 인터뷰와\n감독, 배우님들이 함께하는 온라인 토크콘서트 바로가기 GO &gt;\n- https://t.co/Ycva5UWy5K https://t.co/5uFJd2dJMw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "도서관같은곳",
+					"indices": [
+						27,
+						34
+					]
+				},
+				{
+					"text": "공부하듯이다녔던",
+					"indices": [
+						35,
+						44
+					]
+				},
+				{
+					"text": "희망을주는것",
+					"indices": [
+						45,
+						52
+					]
+				},
+				{
+					"text": "새로운자극",
+					"indices": [
+						53,
+						59
+					]
+				},
+				{
+					"text": "애인같은존재",
+					"indices": [
+						60,
+						67
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Ycva5UWy5K",
+					"expanded_url": "https://www.youtube.com/c/KoreanFilm100years",
+					"display_url": "youtube.com/c/KoreanFilm10…",
+					"indices": [
+						155,
+						178
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1465996978320736300,
+					"id_str": "1465996978320736259",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1465996978320736259/pu/img/EY2-bp-XWGRKnWPl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1465996978320736259/pu/img/EY2-bp-XWGRKnWPl.jpg",
+					"url": "https://t.co/5uFJd2dJMw",
+					"display_url": "pic.twitter.com/5uFJd2dJMw",
+					"expanded_url": "https://twitter.com/SaveOurCinemaKr/status/1466000952389894144/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1465996978320736300,
+					"id_str": "1465996978320736259",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1465996978320736259/pu/img/EY2-bp-XWGRKnWPl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1465996978320736259/pu/img/EY2-bp-XWGRKnWPl.jpg",
+					"url": "https://t.co/5uFJd2dJMw",
+					"display_url": "pic.twitter.com/5uFJd2dJMw",
+					"expanded_url": "https://twitter.com/SaveOurCinemaKr/status/1466000952389894144/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1920,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							16,
+							9
+						],
+						"duration_millis": 63063,
+						"variants": [
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465996978320736259/pu/vid/480x270/5D0gAuCk9nNIj7LJ.mp4?tag=12"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465996978320736259/pu/vid/640x360/XBJ41Opy6gz3cTEQ.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1465996978320736259/pu/vid/1280x720/72mPWh3vkSCF9k4-.mp4?tag=12"
+							},
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1465996978320736259/pu/pl/TAXCTKr7DCtzo46U.m3u8?tag=12&container=fmp4"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1256240129644982300,
+			"id_str": "1256240129644982272",
+			"name": "SaveOurCinema",
+			"screen_name": "SaveOurCinemaKr",
+			"location": "",
+			"description": "#SaveOurCinema\n#BelieveOurPower\n독립·예술영화전용관 응원 캠페인\n-\n\"우리의 극장은,\n우리의 영화적 순간은\n아직 무너지지 않는다\"🙂🎞✨\n \n▼사연이벤트 응모하고 골든패스 GET ▼",
+			"url": "https://t.co/ee0sXjThv1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ee0sXjThv1",
+							"expanded_url": "https://linktr.ee/saveourcinema",
+							"display_url": "linktr.ee/saveourcinema",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 304,
+			"friends_count": 191,
+			"listed_count": 2,
+			"created_at": "Fri May 01 15:12:52 +0000 2020",
+			"favourites_count": 148,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 74,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1259654199370846208/ef2k9mev_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1259654199370846208/ef2k9mev_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1256240129644982272/1589160747",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 39,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "영화의 모든 순간에 당신이 있었다👨‍👩‍👧‍👦\n\n#도서관같은곳 #공부하듯이다녔던\n#희망을주는것 #새로운자극 #애인같은존재\n\n영화인들의 릴레이 인터뷰 미리보기👀\n\n** 고레에다 히로카즈 감독님 특별 인터뷰와\n감독, 배우님들이 함께하는 온라인 토크콘서트 바로가기 GO &gt;\n- https://t.co/Ycva5UWy5K https://t.co/5uFJd2dJMw"
+	},
+	{
 		"created_at": "Wed Dec 01 17:22:42 +0000 2021",
 		"id": 1466095423202857000,
 		"id_str": "1466095423202856962",
