0a31aa3d5458177e1b358c9457d2c9f36e92b6b6
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2022-05-11 09:13:06 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18759,
-	"created_at": "Tue May 10 00:13:05 +0000 2022",
+	"tweets": 18761,
+	"created_at": "Wed May 11 00:13:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_05.js
+++ data/js/tweets/2022_05.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2022_05 = 
 [
 	{
+		"created_at": "Tue May 10 05:40:49 +0000 2022",
+		"id": 1523899884314660900,
+		"id_str": "1523900847402278912",
+		"full_text": "\"여러분 고맙습니다. 드디어 제가 살던 동네로 돌아왔습니다\"\nhttps://t.co/01xH3va9kW https://t.co/OydYa93M7k",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			57
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/01xH3va9kW",
+					"expanded_url": "https://youtu.be/7PE2FAxk6uk",
+					"display_url": "youtu.be/7PE2FAxk6uk",
+					"indices": [
+						34,
+						57
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1523899312584855600,
+					"id_str": "1523899312584855554",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1523899312584855554/pu/img/bTDlJ6S-gMusioNl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1523899312584855554/pu/img/bTDlJ6S-gMusioNl.jpg",
+					"url": "https://t.co/OydYa93M7k",
+					"display_url": "pic.twitter.com/OydYa93M7k",
+					"expanded_url": "https://twitter.com/siniooooo/status/1523899884314660865/video/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1276,
+							"h": 718,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1523899312584855600,
+					"id_str": "1523899312584855554",
+					"indices": [
+						58,
+						81
+					],
+					"media_url": "http://pbs.twimg.com/ext_tw_video_thumb/1523899312584855554/pu/img/bTDlJ6S-gMusioNl.jpg",
+					"media_url_https": "https://pbs.twimg.com/ext_tw_video_thumb/1523899312584855554/pu/img/bTDlJ6S-gMusioNl.jpg",
+					"url": "https://t.co/OydYa93M7k",
+					"display_url": "pic.twitter.com/OydYa93M7k",
+					"expanded_url": "https://twitter.com/siniooooo/status/1523899884314660865/video/1",
+					"type": "video",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1276,
+							"h": 718,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							638,
+							359
+						],
+						"duration_millis": 131186,
+						"variants": [
+							{
+								"content_type": "application/x-mpegURL",
+								"url": "https://video.twimg.com/ext_tw_video/1523899312584855554/pu/pl/jomj3LVNrktnoZjG.m3u8?tag=12&container=fmp4"
+							},
+							{
+								"bitrate": 832000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1523899312584855554/pu/vid/638x360/y28jAhq2ykFAazc0.mp4?tag=12"
+							},
+							{
+								"bitrate": 256000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1523899312584855554/pu/vid/478x270/9PnzonRFEneB0YjW.mp4?tag=12"
+							},
+							{
+								"bitrate": 2176000,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/ext_tw_video/1523899312584855554/pu/vid/1276x718/kLRoEj7irZ1iEhT0.mp4?tag=12"
+							}
+						]
+					},
+					"additional_media_info": {
+						"monetizable": false
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1374343293077651500,
+			"id_str": "1374343293077651457",
+			"name": "시니오",
+			"screen_name": "siniooooo",
+			"location": "",
+			"description": "부계",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2738,
+			"friends_count": 901,
+			"listed_count": 9,
+			"created_at": "Tue Mar 23 12:53:14 +0000 2021",
+			"favourites_count": 7904,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 7452,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1508420413428477956/oR7VKnVh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1508420413428477956/oR7VKnVh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1374343293077651457/1616506012",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 715,
+		"favorite_count": 1199,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "\"여러분 고맙습니다. 드디어 제가 살던 동네로 돌아왔습니다\"\nhttps://t.co/01xH3va9kW https://t.co/OydYa93M7k"
+	},
+	{
+		"created_at": "Tue May 10 05:40:44 +0000 2022",
+		"id": 1523900824979607600,
+		"id_str": "1523900824979607552",
+		"full_text": "달봉이 어제 첫개시하고 개뿌듯",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			16
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel 맥거핀 펭수🐧💙 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 605,
+			"friends_count": 1109,
+			"listed_count": 14,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 2070,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 18937,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "달봉이 어제 첫개시하고 개뿌듯"
+	},
+	{
 		"created_at": "Mon May 09 23:20:50 +0000 2022",
 		"id": 1523805223105753000,
 		"id_str": "1523805223105753089",
