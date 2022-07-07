ab3887c4c0a108e5214a98176382c686bcea73ac
sungeunyun <35427800+sungeunyun@users.noreply.github.com>
2022-07-07 09:13:05 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18770,
-	"created_at": "Fri Jul 01 00:13:05 +0000 2022",
+	"tweets": 18771,
+	"created_at": "Thu Jul 07 00:13:05 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_07.js
@@ -0,0 +1,188 @@
+Grailbird.data.tweets_2022_07 = 
+[
+	{
+		"created_at": "Thu Jul 07 00:06:04 +0000 2022",
+		"id": 1544622104863056000,
+		"id_str": "1544835102165078016",
+		"full_text": "‘어 제 배우 누구지’ 했던 그 배우가 바로 나야 기루기루 강길우! 브로커 소영(아이유)에게 욕 싸다구 맞은 바로 그 사람! 이상청에서 “장관님 팬입니다” 외치던 바로 그 사람! 기억난다면 보러오지 않겠기루? 이번 가을 부산으로! https://t.co/23rKcgqbGB https://t.co/hZKKgsHVR2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/23rKcgqbGB",
+					"expanded_url": "http://community.biff.kr/kor/addon/00000005/infowin.asp?intNum=14360",
+					"display_url": "community.biff.kr/kor/addon/0000…",
+					"indices": [
+						129,
+						152
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1544621907982446600,
+					"id_str": "1544621907982446593",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FW-ZcQmUcAEIqqd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FW-ZcQmUcAEIqqd.jpg",
+					"url": "https://t.co/hZKKgsHVR2",
+					"display_url": "pic.twitter.com/hZKKgsHVR2",
+					"expanded_url": "https://twitter.com/caritas87/status/1544622104863055873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1544621907982446600,
+					"id_str": "1544621907982446593",
+					"indices": [
+						153,
+						176
+					],
+					"media_url": "http://pbs.twimg.com/media/FW-ZcQmUcAEIqqd.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FW-ZcQmUcAEIqqd.jpg",
+					"url": "https://t.co/hZKKgsHVR2",
+					"display_url": "pic.twitter.com/hZKKgsHVR2",
+					"expanded_url": "https://twitter.com/caritas87/status/1544622104863055873/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1080,
+							"h": 1080,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 142993164,
+			"id_str": "142993164",
+			"name": "배부르주아🎗",
+			"screen_name": "caritas87",
+			"location": "영화관",
+			"description": "귀여움수집가 🐧🎬  그런데 늘 그랬지만 요즘은 더더욱 구교환 얘기만 함",
+			"url": "https://t.co/9jEGWiiZm8",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9jEGWiiZm8",
+							"expanded_url": "https://peing.net/ko/caritas87",
+							"display_url": "peing.net/ko/caritas87",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 501,
+			"friends_count": 228,
+			"listed_count": 4,
+			"created_at": "Wed May 12 08:41:22 +0000 2010",
+			"favourites_count": 7083,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 39597,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1538516692514398208/HOugKzAv_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1538516692514398208/HOugKzAv_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/142993164/1639716192",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "‘어 제 배우 누구지’ 했던 그 배우가 바로 나야 기루기루 강길우! 브로커 소영(아이유)에게 욕 싸다구 맞은 바로 그 사람! 이상청에서 “장관님 팬입니다” 외치던 바로 그 사람! 기억난다면 보러오지 않겠기루? 이번 가을 부산으로! https://t.co/23rKcgqbGB https://t.co/hZKKgsHVR2"
+	}
+]
