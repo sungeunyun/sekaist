c6b8600ef885c7626098ce3cb2e54c8c44a58595
sekaist <35427800+sungeunyun@users.noreply.github.com>
2022-09-07 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18829,
-	"created_at": "Tue Aug 30 00:13:05 +0000 2022",
+	"tweets": 18830,
+	"created_at": "Wed Sep 07 00:13:05 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_09.js
@@ -0,0 +1,86 @@
+Grailbird.data.tweets_2022_09 = 
+[
+	{
+		"created_at": "Tue Sep 06 13:06:08 +0000 2022",
+		"id": 1567137070292029400,
+		"id_str": "1567137070292029440",
+		"full_text": "오피스텔 비상계단 쪽 엘리베이터를 타려고 문을 열었더니 누군가 타고 있어서 후다닥 뛰어들어갔는데 그분이 정동진영화제 티셔츠 입고 계셔서 깜놀 심지어 19년도 티셔츠고 나는 이번에 그 티 개시해서 입고 정동진영화제 갔었는데.. 정동진 다녀온 주민을 마주쳐서 넘나 신기",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			148
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel 맥거핀 펭수🐧💙 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 601,
+			"friends_count": 1110,
+			"listed_count": 14,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 2067,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 19006,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "ko",
+		"text": "오피스텔 비상계단 쪽 엘리베이터를 타려고 문을 열었더니 누군가 타고 있어서 후다닥 뛰어들어갔는데 그분이 정동진영화제 티셔츠 입고 계셔서 깜놀 심지어 19년도 티셔츠고 나는 이번에 그 티 개시해서 입고 정동진영화제 갔었는데.. 정동진 다녀온 주민을 마주쳐서 넘나 신기"
+	}
+]
