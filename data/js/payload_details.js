df65ca2fa2a398f9b7986f883150fb5acd60927d
sekaist <35427800+sungeunyun@users.noreply.github.com>
2022-10-17 09:13:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18869,
-	"created_at": "Sun Oct 16 00:13:05 +0000 2022",
+	"tweets": 18870,
+	"created_at": "Mon Oct 17 00:13:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_10.js
+++ data/js/tweets/2022_10.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2022_10 = 
 [
 	{
+		"created_at": "Sun Oct 16 04:06:52 +0000 2022",
+		"id": 1581496875789975600,
+		"id_str": "1581496875789975553",
+		"full_text": "페스티벌 가면서 티켓 안챙겨와서 돌아갔음 10년전에 지산갈때도 그랬는데.... 그땐 지산 도착해서 알았지만 오늘은 지하철 한정거장 지나서 돌아갔다 https://t.co/IKuRLbX6id",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			81
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1581496871457263600,
+					"id_str": "1581496871457263617",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FfKbBoSaEAEw4UL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfKbBoSaEAEw4UL.jpg",
+					"url": "https://t.co/IKuRLbX6id",
+					"display_url": "pic.twitter.com/IKuRLbX6id",
+					"expanded_url": "https://twitter.com/sekaist/status/1581496875789975553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1581496871457263600,
+					"id_str": "1581496871457263617",
+					"indices": [
+						82,
+						105
+					],
+					"media_url": "http://pbs.twimg.com/media/FfKbBoSaEAEw4UL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FfKbBoSaEAEw4UL.jpg",
+					"url": "https://t.co/IKuRLbX6id",
+					"display_url": "pic.twitter.com/IKuRLbX6id",
+					"expanded_url": "https://twitter.com/sekaist/status/1581496875789975553/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 400,
+							"h": 334,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 133336572,
+			"id_str": "133336572",
+			"name": "캥거루",
+			"screen_name": "sekaist",
+			"location": "South Korea",
+			"description": "Don't get sentimental. It always ends up drivel 맥거핀 펭수🐧💙 ㄷㅡ6",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 595,
+			"friends_count": 1104,
+			"listed_count": 14,
+			"created_at": "Thu Apr 15 15:02:16 +0000 2010",
+			"favourites_count": 2074,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 19058,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0099B9",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3218324254/443f8cf3a23d28cbd7ceef010f2f3000_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/133336572/1492614623",
+			"profile_link_color": "0099B9",
+			"profile_sidebar_border_color": "5ED4DC",
+			"profile_sidebar_fill_color": "95E8EC",
+			"profile_text_color": "3C3940",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "페스티벌 가면서 티켓 안챙겨와서 돌아갔음 10년전에 지산갈때도 그랬는데.... 그땐 지산 도착해서 알았지만 오늘은 지하철 한정거장 지나서 돌아갔다 https://t.co/IKuRLbX6id"
+	},
+	{
 		"created_at": "Sat Oct 15 14:33:04 +0000 2022",
 		"id": 1581292074674323500,
 		"id_str": "1581292074674323457",
