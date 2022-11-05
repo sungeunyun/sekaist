a7ffb679c1c5d556adf25febb2cbbf6ed4d8075a
sekaist <35427800+sungeunyun@users.noreply.github.com>
2022-11-05 09:13:12 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18903,
-	"created_at": "Fri Nov 04 00:13:05 +0000 2022",
+	"tweets": 18904,
+	"created_at": "Sat Nov 05 00:13:10 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Fri Nov 04 14:20:53 +0000 2022",
+		"id": 1588535796063408000,
+		"id_str": "1588536765178327042",
+		"full_text": "[속보] 당국 \"봉화 광산 고립자 2명 생환…지상으로 걸어 나와\" https://t.co/ZYhxQWpMBv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ZYhxQWpMBv",
+					"expanded_url": "https://m.yna.co.kr/view/AKR20221104156100053",
+					"display_url": "m.yna.co.kr/view/AKR202211…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 63764417,
+			"id_str": "63764417",
+			"name": "푸른곰",
+			"screen_name": "purengom",
+			"location": "Gyeonggi Province, South Korea",
+			"description": "IT블로거. 맥, 윈도우, iOS, 안드로이드 사용중. MCP. 고양이와 커피, 하겐다즈 좋아하는 잡식성 오타쿠. 국제 뉴스나 IT 소식/트렌드, 애니, 서브컬처, 일상 위주 트윗. 모든 팔로우/RT/링크/좋아요가 찬동/추천의 의미는 아니며 멘션등에 전부는 대답 못드림을 양해 바랍니다.",
+			"url": "https://t.co/b2CbWTM5GT",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b2CbWTM5GT",
+							"expanded_url": "https://purengom.com",
+							"display_url": "purengom.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4275,
+			"friends_count": 3760,
+			"listed_count": 120,
+			"created_at": "Fri Aug 07 16:24:30 +0000 2009",
+			"favourites_count": 124294,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 254368,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1245327825759924230/dD30UhPq_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1245327825759924230/dD30UhPq_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/63764417/1446705317",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2762,
+		"favorite_count": 852,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "[속보] 당국 \"봉화 광산 고립자 2명 생환…지상으로 걸어 나와\" https://t.co/ZYhxQWpMBv"
+	},
+	{
 		"created_at": "Thu Nov 03 11:58:30 +0000 2022",
 		"id": 1588138549043691500,
 		"id_str": "1588138549043691522",
